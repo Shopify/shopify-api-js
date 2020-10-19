e1afa3465a8af53b829fa45652f0a64491abf67a
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2020-10-19 11:19:27 -0400

    Directory framework

--- src/world/hello-goodbye.ts
+++
@@ -1,6 +0,0 @@
-export function sayHello(name: String) {
-  return 'Hello, ' + name;
-}
-export function sayGoodbye(name: String) {
-  return 'Goodbye, ' + name;
-}
--- src/world/test/hello-goodbye.test.ts
+++
@@ -1,9 +0,0 @@
-const hg = require('../hello-goodbye');
-
-test('response from hello', () => {
-  expect(hg.sayHello('test')).toMatch('Hello, test');
-});
-
-test('response from goodbye', () => {
-  expect(hg.sayGoodbye('test')).toMatch('Goodbye, test');
-});
