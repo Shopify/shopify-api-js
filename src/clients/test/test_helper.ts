35f8800972155872854b16bde55e5d1b95fff53a
Paulo Margarido <paulo.margarido@shopify.com>
2020-11-16 12:05:38 -0500

    Replace axios with node-fetch to make HTTP requests

--- src/clients/test/test_helper.ts
+++ src/clients/test/test_helper.ts
@@ -1,28 +1,19 @@
 import querystring, { ParsedUrlQueryInput } from 'querystring';
-
-import axios from 'axios';
-jest.mock('axios');
-export const axiosMock = axios as jest.Mocked<typeof axios>;
-
-export function buildHttpResponse(data: unknown): Promise<unknown> {
-  return Promise.resolve({
-    status: 200,
-    data: data,
-  });
-}
+import fetchMock from  'jest-fetch-mock';
 
 export function assertHttpRequest(
   method: string,
   domain: string,
-  url: string,
+  path: string,
   headers: Record<string, unknown> = {},
   data: ParsedUrlQueryInput | null = null
 ): void {
-  expect(axiosMock.request).toBeCalledWith(expect.objectContaining({
-    method: method,
-    baseURL: `https://${domain}`,
-    url: url,
-    headers: expect.objectContaining(headers),
-    data: data ? querystring.stringify(data) : null,
-  }));
+  expect(fetchMock).toBeCalledWith(
+    `https://${domain}${path}`,
+    expect.objectContaining({
+      method: method,
+      headers: expect.objectContaining(headers),
+      body: data ? querystring.stringify(data) : null,
+    })
+  );
 }
