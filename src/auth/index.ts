3c45acb161ade755c12dd8157ba0d293750b9830
Paulo Margarido <paulo.margarido@shopify.com>
2021-01-15 17:13:36 -0500

    Fixing last linting issues

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -2,7 +2,8 @@ import ShopifyOAuth from './oauth';
 import * as Session from './session';
 
 const ShopifyAuth = {
-  OAuth: ShopifyOAuth, // Not entirely satisfied with this as we can't export more objects from the oauth module
+  // Not entirely satisfied with this as we can't export more objects from the oauth module
+  OAuth: ShopifyOAuth,
   Session,
 };
 
