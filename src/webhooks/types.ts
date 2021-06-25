4f1be76f2e9b224c7ca96935b7ab8fc7d4504667
Paulina Khew <paulina.khew@shopify.com>
2021-06-25 11:52:09 -0400

    Add addHandler and getHandler functions

--- src/webhooks/types.ts
+++ src/webhooks/types.ts
@@ -16,7 +16,7 @@ export interface RegisterOptions {
   path: string;
   shop: string;
   accessToken: string;
-  webhookHandler: WebhookHandlerFunction;
+  webhookHandler?: WebhookHandlerFunction;
   deliveryMethod?: DeliveryMethod;
 }
 
