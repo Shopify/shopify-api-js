7b108e006fd19c5b5fee53c53c8880a50a7f74fa
Dan King <danwd.king@shopify.com>
2021-09-29 16:03:03 -0400

    Added regex for spin in the shop URL validator

--- src/utils/shop-validator.ts
+++ src/utils/shop-validator.ts
@@ -4,6 +4,6 @@
  * @param shop Shop url: {shop}.myshopify.com
  */
 export default function validateShop(shop: string): boolean {
-  const shopUrlRegex = /^[a-zA-Z0-9][a-zA-Z0-9-]*\.myshopify\.(com|io)[/]*$/;
+  const shopUrlRegex = /(^[a-zA-Z0-9][a-zA-Z0-9-]*\.myshopify\.(com|io)[/]*$|^[a-zA-Z0-9][a-zA-Z0-9-]*\.shopify\.[a-zA-Z0-9][a-zA-Z0-9-]*\.[a-zA-Z0-9][a-zA-Z0-9-]*\.(us|asia|eu)\.spin.dev[/]*$)/;
   return shopUrlRegex.test(shop);
 }
