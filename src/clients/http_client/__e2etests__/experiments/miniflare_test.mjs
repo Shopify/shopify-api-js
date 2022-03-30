c182facdf0d56248b8a0f1db8c085797bac5ef6d
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2022-03-29 23:06:56 -0400

    Deletes unneeded lines

--- src/clients/http_client/__e2etests__/experiments/miniflare_test.mjs
+++ src/clients/http_client/__e2etests__/experiments/miniflare_test.mjs
@@ -4,17 +4,9 @@ import { Miniflare } from "miniflare";
 
 const mf = new Miniflare({
   buildCommand: "yarn e2etests:build:cf_worker", // Command to build project
-  buildBasePath: "bundle", // Working directory for build command
   scriptPath: 'bundle/http_client-cf-worker.test.js',
   envPath: "src/clients/http_client/__e2etests__/.env.test",
-  // scriptPath: 'src/clients/http_client/__e2etests__/http_client-cf-worker.test.js',
-  // packagePath: 'src/clients/http_client/__e2etests__/package.e2etests.json',
   modules: true,
-  modulesRules: [
-    // Modules import rule
-    { type: "CommonJS", include: ["**/*.js", "**/*.ts"], fallthrough: true },
-    // { type: "ESModule", include: ["**/*.js", "**/*.ts"], fallthrough: true },
-  ],
 });
 const res = await mf.dispatchFetch("http://localhost:8787/");
 console.log(await res.text());
