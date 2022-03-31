bc8d252be04598184ad6f1cb3de9790fa2488b86
Derek Szeto <derek.szeto@shopify.com>
2022-03-31 19:56:32 +0000

    Modify shop validator to support Spin urls

---
+++ src/utils/shop-validator.ts
@@ -0,0 +1,10 @@
+/**
+ * Validates myshopify.com urls
+ *
+ * @param shop Shop url: {shop}.myshopify.com
+ */
+export default function validateShop(shop: string): boolean {
+  const shopUrlRegex =
+    /(^[a-zA-Z0-9][a-zA-Z0-9-]*\.myshopify\.(com|io)[/]*$|^[a-zA-Z0-9][a-zA-Z0-9-]*\.shopify\.[a-zA-Z0-9][a-zA-Z0-9-]*\.[a-zA-Z0-9][a-zA-Z0-9-]*\.(us|asia|eu)\.spin.dev[/]*$)/;
+  return shopUrlRegex.test(shop);
+}
