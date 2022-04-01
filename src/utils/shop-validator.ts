a721423ec88e90ceb3342c760d8652526c2f72dc
Derek Szeto <derek.szeto@shopify.com>
2022-04-01 14:55:15 +0000

    Split regexes into different variables as per review

--- src/utils/shop-validator.ts
+++ src/utils/shop-validator.ts
@@ -4,7 +4,12 @@
  * @param shop Shop url: {shop}.myshopify.com
  */
 export default function validateShop(shop: string): boolean {
-  const shopUrlRegex =
-    /(^[a-zA-Z0-9][a-zA-Z0-9-]*\.myshopify\.(com|io)[/]*$|^[a-zA-Z0-9][a-zA-Z0-9-]*\.shopify\.[a-zA-Z0-9][a-zA-Z0-9-]*\.[a-zA-Z0-9][a-zA-Z0-9-]*\.(us|asia|eu)\.spin.dev[/]*$)/;
-  return shopUrlRegex.test(shop);
+  const shopUrlRegex = /^[a-zA-Z0-9][a-zA-Z0-9-]*\.myshopify\.(com|io)[/]*$/;
+  const spinUrlRegex =
+    /^[a-zA-Z0-9][a-zA-Z0-9-]*\.shopify\.[a-zA-Z0-9][a-zA-Z0-9-]*\.[a-zA-Z0-9][a-zA-Z0-9-]*\.(us|asia|eu)\.spin.dev[/]*$/;
+  const combinedRegex = new RegExp(
+    `${shopUrlRegex.source}|${spinUrlRegex.source}`,
+  );
+
+  return combinedRegex.test(shop);
 }
