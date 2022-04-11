baceae76cc03b6a078637f1ba6996a9b5a7c2ad2
Surma <surma@shopify.com>
2022-04-11 11:31:25 +0100

    SQlite3 adapter

---
+++ src/auth/session/storage/sqlite.ts
@@ -0,0 +1,107 @@
+import sqlite3 from 'sqlite3';
+
+import {SessionInterface} from '../types';
+import {SessionStorage} from '../session_storage';
+
+export interface SQLiteSessionStorageOptions {
+  createDBWhenMissing: boolean;
+  sessionTableName: string;
+}
+const defaultMySQLSessionStorageOptions: SQLiteSessionStorageOptions = {
+  createDBWhenMissing: true,
+  sessionTableName: 'shopify_node_api_sessions',
+};
+
+export class SQLiteSessionStorage implements SessionStorage {
+  private options: SQLiteSessionStorageOptions;
+  private db: sqlite3.Database;
+
+  constructor(
+    private filename: string,
+    opts: Partial<SQLiteSessionStorageOptions>,
+  ) {
+    this.options = {...defaultMySQLSessionStorageOptions, ...opts};
+    this.db = new sqlite3.Database(this.filename);
+    this.init();
+  }
+
+  public async hasSessionTable(): Promise<boolean> {
+    const query = sql`
+    SELECT name FROM sqlite_schema
+    WHERE
+      type = "table" AND
+      name = ${JSON.stringify(this.options.sessionTableName)}
+    `;
+    const rows = await this.query(query);
+    return rows.length === 1;
+  }
+
+  public async storeSession(session: SessionInterface): Promise<boolean> {
+    const id = session.id;
+    const payload = JSON.stringify(session);
+
+    const query = sql`
+      INSERT INTO ${this.options.sessionTableName}
+      VALUES (${JSON.stringify(id)}, '${payload.replace(/"'"/g, "''")}');
+    `;
+    console.log({query});
+    await this.query(query);
+    return true;
+  }
+
+  public async loadSession(id: string): Promise<SessionInterface | undefined> {
+    const query = sql`
+      SELECT payload FROM ${this.options.sessionTableName}
+      WHERE id = ${JSON.stringify(id)};
+    `;
+    const result = await this.query(query);
+    if (!Array.isArray(result) || result?.length !== 1) return undefined;
+    return JSON.parse((result[0] as any).payload);
+  }
+
+  public async deleteSession(id: string): Promise<boolean> {
+    const query = sql`
+      DELETE FROM ${this.options.sessionTableName}
+      WHERE id = ${JSON.stringify(id)};
+    `;
+    await this.query(query);
+    return true;
+  }
+
+  private async init() {
+    const hasSessionTable = await this.hasSessionTable();
+    if (!hasSessionTable && !this.options.createDBWhenMissing) {
+      throw Error('Session Table is missing');
+    } else if (!hasSessionTable) {
+      const query = sql`
+        CREATE TABLE ${this.options.sessionTableName} (
+          id varchar(255) NOT NULL PRIMARY KEY,
+          payload varchar(4095) NOT NULL
+        )
+      `;
+      await this.query(query);
+    }
+  }
+
+  private query(sql: string): Promise<any[]> {
+    return new Promise((resolve, reject) => {
+      this.db.all(sql, (err, result) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(result);
+      });
+    });
+  }
+}
+
+/**
+ * Quick’n’dirty tagged template literal to allow string interpolation with
+ * backticks without conflicting with SQL’s usage of backticks.
+ * It effectively replaces single quotes with backticks,
+ * leaving double quotes for strings.
+ */
+function sql(raw: Parameters<typeof String.raw>[0], ...substitutions: any[]) {
+  return String.raw({raw} as any, ...substitutions);
+}
