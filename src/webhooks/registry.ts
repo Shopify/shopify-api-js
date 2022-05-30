099787a6d43337cbbc7cfb61c61fbbdf63fedb5a
Ben Levy <ben@foxhound.systems>
2022-05-30 18:33:44 -0500

    Fix StatusCode on Invalid Signature
    Return 401 Unauthorized instead of 403 Forbidden when the signature is invalid. 
    
    This error was reported in our App submission. There should probably be something in the documentation specifying that you MUST return a 401.

--- src/webhooks/registry.ts
+++ src/webhooks/registry.ts
@@ -428,7 +428,7 @@ const WebhooksRegistry: RegistryInterface = {
             );
           }
         } else {
-          statusCode = StatusCode.Forbidden;
+          statusCode = StatusCode.Unauthorized;
           responseError = new ShopifyErrors.InvalidWebhookError(
             `Could not validate request for topic ${topic}`,
           );
