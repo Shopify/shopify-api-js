afeb0d4972de02594ec511176bc04dd8cdbce5de
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2022-07-04 10:56:47 -0400

    Remove double-export

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,9 @@ import ShopifySession from './auth/session';
 import ShopifyClients from './clients';
 import ShopifyUtils from './utils';
 import ShopifyWebhooks from './webhooks';
-import {LATEST_API_VERSION} from './base-types';
 
 export const Shopify = {
   Context,
-  LATEST_API_VERSION,
   Auth: ShopifyAuth,
   Session: ShopifySession,
   Clients: ShopifyClients,
