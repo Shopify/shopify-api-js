5179891fb0093b1565bd9b1ee92b1a4378c596ad
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2022-08-22 09:57:04 -0400

    Extract crypto test random into module

--- src/runtime/crypto/__tests__/produce-node-hmac.mjs
+++ src/runtime/crypto/__tests__/produce-node-hmac.mjs
@@ -1,12 +1,6 @@
 import crypto from 'crypto';
 
-function random({min = 0, max = 1, floor = true} = {}) {
-  let value = Math.random() * (max - min) + min;
-  if (floor) {
-    value = Math.floor(value);
-  }
-  return value;
-}
+import {random} from './random.mjs';
 
 const NUM_FUZZS = parseInt(process.argv[2]);
 
