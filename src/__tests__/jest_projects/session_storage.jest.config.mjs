af95a8fa47fffce826eb052aab89d2fc609ede66
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2022-09-30 11:20:01 -0400

    Add KV session storage

--- src/__tests__/jest_projects/session_storage.jest.config.mjs
+++ src/__tests__/jest_projects/session_storage.jest.config.mjs
@@ -1,7 +1,15 @@
 import baseConfig from './base.jest.config.mjs';
 
+import semver from 'semver';
+
+const ignorePaths = [];
+if (semver.lt(process.version, '15.0.0')) {
+  ignorePaths.push('src/session-storage/__tests__/kv.test.ts');
+}
+
 export default {
   ...baseConfig,
   displayName: 'session_storage',
   rootDir: '../../session-storage',
+  testPathIgnorePatterns: ignorePaths,
 };
