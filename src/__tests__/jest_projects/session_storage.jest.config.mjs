2f6f101497335823ddacd4fc04eb6ca623e06326
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2022-10-03 10:21:51 -0400

    Update session_storage.jest.config.mjs

--- src/__tests__/jest_projects/session_storage.jest.config.mjs
+++ src/__tests__/jest_projects/session_storage.jest.config.mjs
@@ -2,6 +2,8 @@ import baseConfig from './base.jest.config.mjs';
 
 import semver from 'semver';
 
+// eslint-disable-next-line no-warning-comments
+// TODO Remove this when dropping Node 14 support
 const ignorePaths = [];
 if (semver.lt(process.version, '15.0.0')) {
   ignorePaths.push('src/session-storage/__tests__/kv.test.ts');
