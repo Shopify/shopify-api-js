52cc97bc90d0b414bf0f43c548baf9832a263145
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2023-04-28 20:55:37 -0400

    Remove support for Node 14
    This requires removing support for Node 14, which is EOL, which in turn
    means we can use newer crypto APIs.

--- lib/auth/oauth/nonce.ts
+++ lib/auth/oauth/nonce.ts
@@ -1,13 +1,12 @@
 import {crypto} from '../../../runtime/crypto';
 
 export function nonce(): string {
+  const cryptoLib =
+    typeof (crypto as any)?.webcrypto === 'undefined'
+      ? crypto
+      : (crypto as any).webcrypto;
   const length = 15;
-
-  // eslint-disable-next-line no-warning-comments
-  // TODO Remove the randomBytes call when dropping Node 14 support
-  const bytes = crypto.getRandomValues
-    ? crypto.getRandomValues(new Uint8Array(length))
-    : (crypto as any).randomBytes(length);
+  const bytes = cryptoLib.getRandomValues(new Uint8Array(length));
 
   const nonce = bytes
     .map((byte: number) => {
