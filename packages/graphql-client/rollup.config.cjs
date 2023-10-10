b074c5a045fc8db6eae07b01673dd322d2509ccb
Melissa Luu <melissa.luu@shopify.com>
2023-10-10 11:40:09 -0400

    Add missing rollup, eslint and prettier files

---
+++ packages/graphql-client/rollup.config.cjs
@@ -0,0 +1,52 @@
+import dts from 'rollup-plugin-dts';
+import {getPlugins, mainSrcInput, bannerConfig} from './rollup-common.cjs';
+
+export default [
+  {
+  input: mainSrcInput,
+  plugins: getPlugins({
+    minify: true
+  }),
+  output: [
+    {
+      file: './dist/graphql-client.min.js',
+      format: 'umd',
+      sourcemap: true,
+      name: 'ShopifyGraphQLClient',
+      ...bannerConfig
+    },
+  ]
+},
+{
+  input: mainSrcInput,
+  plugins: getPlugins(),
+  output: [
+    {
+      dir: './dist',
+      format: 'es',
+      sourcemap: true,
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      entryFileNames: '[name].mjs',
+    },
+  ]
+},
+{
+  input: mainSrcInput,
+  plugins: getPlugins(),
+  output: [
+    {
+      dir: './dist',
+      format: 'cjs',
+      sourcemap: true,
+      exports: "named",
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+    },
+  ]
+},
+{
+  input: './dist/ts/index.d.ts',
+  output: [{ file: 'dist/graphql-client.d.ts', format: 'es' }],
+  plugins: [dts.default()],
+}];
