89965e1fd13a730f81ffc0160112a45b87added3
Rezaan Syed <rezaan.syed@shopify.com>
2023-11-28 12:05:09 -0500

    Add isExpired() to Session object

--- packages/shopify-api/lib/session/__tests__/session.test.ts
+++ packages/shopify-api/lib/session/__tests__/session.test.ts
@@ -74,6 +74,47 @@ describe('isActive', () => {
   });
 });
 
+describe('isExpired', () => {
+  it('returns true if session is expired', () => {
+    const session = new Session({
+      id: 'not_active',
+      shop: 'inactive-shop',
+      state: 'not_same',
+      isOnline: true,
+      scope: 'test_scope',
+      expires: new Date(Date.now() - 1),
+    });
+    expect(session.isExpired()).toBeTruthy();
+  });
+
+  it('returns false if session is expired', () => {
+    const session = new Session({
+      id: 'active',
+      shop: 'active-shop',
+      state: 'test_state',
+      isOnline: true,
+      scope: 'test_scope',
+      accessToken: 'indeed',
+      expires: new Date(Date.now() + 86400),
+    });
+
+    expect(session.isExpired()).toBeFalsy();
+  });
+
+  it('returns false if session does not have expiry', () => {
+    const session = new Session({
+      id: 'active',
+      shop: 'active-shop',
+      state: 'test_state',
+      isOnline: true,
+      scope: 'test_scope',
+      accessToken: 'indeed',
+    });
+
+    expect(session.isExpired()).toBeFalsy();
+  });
+});
+
 const expiresDate = new Date(Date.now() + 86400);
 const expiresNumber = expiresDate.getTime();
 
